50325d893b2022be520cff7ac9c3fd2855300a9c
YutaSugimura <nextc3m@gmail.com>
2018-11-01 00:22:58 +0900

    main contracts

--- contracts/reward.sol
+++ contracts/reward.sol
@@ -1,8 +1,9 @@
 pragma solidity 0.4.24;
 
 import "./Mint.sol";
+import "./Sponsor.sol";
 
-contract reward is Mint {
+contract reward is Mint, Sponsor {
 
   constructor() public ERC721Full("Blockchain-Nagoya", "BCN") {}
 
