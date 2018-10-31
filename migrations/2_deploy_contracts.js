55f591799999a8514453153a38e0a147683df097
YutaSugimura <nextc3m@gmail.com>
2018-11-01 00:57:43 +0900

    migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-let Contract = artifacts.require("./reward.sol");
+let Contracts = artifacts.require("./reward.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Contract);
+  deployer.deploy(Contracts);
 };
