92f0872d0b54f458405e1d07d8f3cfebee414843
YutaSugimura <nextc3m@gmail.com>
2018-11-01 00:58:22 +0900

    meta contracts

--- contracts/Meta.sol
+++ contracts/Meta.sol
@@ -3,6 +3,30 @@ pragma solidity 0.4.24;
 
 contract Meta {
 
+  /*
+    {
+      "title": "gift",
+      "type": "object",
+      "properties": {
+        "class": {
+          "type": "string",
+          "description": "Common",
+        },
+        "name": {
+          "type": "string",
+          "description": "Identifies the asset to which this NFT represents",
+        },
+        "description" : {
+          "type": "string",
+          "description": "Describes the asset to which this NFT represents"
+        },
+        "image": {
+          "type": "string",
+          "description": "URL",
+        }
+      }
+    }
+  */
 
   function setMetaData(string _meta) public {
     
