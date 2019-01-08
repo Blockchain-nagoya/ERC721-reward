9ed0101e4191860966b2eb4232f1e035b2697d9e
{YutaSugimura} <nextc3m@gmail.com>
2019-01-08 18:44:19 +0900

    delete

--- migrations/2_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-let Contracts = artifacts.require("./reward.sol");
-
-module.exports = function(deployer) {
-  let name = "TestToken";
-  let symbol = "TST";
-  deployer.deploy(Contracts, name, symbol);
-};
